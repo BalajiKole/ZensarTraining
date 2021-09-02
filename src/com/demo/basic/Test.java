665a2bfaab6d0393de5b68f22ec1ce74984cfb5e
BalajiKole <66683134+BalajiKole@users.noreply.github.com>
2021-09-02 11:55:22 +0530

    multi two commit

--- src/com/demo/basic/Test.java
+++ src/com/demo/basic/Test.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	int number=100;
 	int numberOne=500;
 	System.out.println(number+numberOne);
+	System.out.println(number*numberOne);
 	System.out.println("Hello Test file");
 	System.out.println("Hi How are you?????");
 }
