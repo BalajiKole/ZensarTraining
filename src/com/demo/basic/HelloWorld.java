5d9c6029d584a67bc3dd3c95874cdfc7a0dcc291
BalajiKole <tqbalaji@gmail.com>
2021-09-02 11:12:48 +0530

    Basic code program

---
+++ src/com/demo/basic/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.demo.basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int numberOne = 100;
+		int numberTwo = 200;
+		int result = numberOne + numberTwo;
+		System.out.println("Addition= " + result);
+		System.out.println("Hello world.........");
+
+	}
+}
